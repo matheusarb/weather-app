0949689eb9ff6c6cef41cf16235ddcb00268a075
matheusarb <mat.araujoribeiro@gmail.com>
2022-11-12 20:24:29 -0300

    initial commit

---
+++ src/components/search/search.js
@@ -0,0 +1,10 @@
+// will utilize the async package installed
+
+const Search = () => {
+  //Search component is a dumb react component
+  //Dumb components just present something to the DOM
+  //They don't have logic inside then, therefore they're called dumb
+  return ("Hello");
+};
+
+export default Search;
