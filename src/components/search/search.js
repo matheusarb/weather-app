27a99e0e964ad9b895703f2897d04a39f323f728
matheusarb <mat.araujoribeiro@gmail.com>
2022-11-14 17:57:45 -0300

    api fetch update

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,24 +1,41 @@
 import React from "react";
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate"; // async package installed for city search
+import { geoApiOptions, GEO_API_URL } from "../api"; // import the two methods from the same file
 
-const Search = ({onSearchChange}) => {
-  
+const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
 
-  const handleOnChange = searchData => {
+  const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
   };
 
-  const loadOptions = () => {
-
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=10000000&namePrefix=&{inputValue}`,
+      geoApiOptions
+    ) // cities?
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name} ${city.countryCode}`,
+            }
+          })
+        }
+      })
+      .catch((err) => console.error(err));
   };
 
   return (
     <AsyncPaginate
       placeholder="Search for City"
-      debounceTimeout={600} /*prevent user to push search button very fast and stack multiple requests*/
+      debounceTimeout={
+        600
+      } /*prevent user to push search button very fast and stack multiple requests*/
       value={search}
       onChange={handleOnChange}
       loadOptions={loadOptions}
