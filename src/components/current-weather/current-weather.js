680f7b5875a49e2e88fe4fe4b4834d4c1c292583
matheusarb <mat.araujoribeiro@gmail.com>
2022-11-14 21:56:35 -0300

    update current-weather component

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -22,7 +22,7 @@ const CurrentWeather = () => {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value">2m p/second</span>
+            <span className="parameter-value">2 m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
